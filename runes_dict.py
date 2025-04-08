261ebee74aefc103126b853350e5c3f86f962d8f
TashiBelle <natasha.olivier224@gmail.com>
2025-04-08 05:35:03 -0500

    Update runes_dict.py

--- runes_dict.py
+++ runes_dict.py
@@ -4,153 +4,164 @@ runes = {
   "FEHU": {
     "phonetic": "F",
     "number": 1,
-    "bright_stave": ["social success", "wealth energy", "foresight", "new beginnings"],
-    "murky_stave": ["greed", "burnout", "atrophy", "poverty", "discord"]
+    "bright": ["social success", "wealth energy", "foresight", "new beginnings"],
+    "murky": ["greed", "burnout", "atrophy", "poverty", "discord"]
   },
   "URUZ": {
     "phonetic": "U",
     "number": 2,
-    "bright_stave": ["strength", "defense", "tenacity", "freedom", "form", "health", "understanding"],
-    "murky_stave": ["weakness", "obsession", "misdirected force", "domination by others", "sickness", "inconsistency", "ignorance"]
+    "bright": ["strength", "defense", "tenacity", "freedom", "form", "health", "understanding"],
+    "murky": ["weakness", "obsession", "misdirected force", "domination by others", "sickness", "inconsistency", "ignorance"]
   },
   "THURISAZ": {
     "phonetic": "TH",
     "number": 3,
-    "bright_stave": ["reactive force", "directed force", "vital eroticism", "regenerative catalyst"],
-    "murky_stave": ["danger", "defenselessness", "compulsion", "betrayal", "dullness"]
+    "bright": ["reactive force", "directed force", "vital eroticism", "regenerative catalyst"],
+    "murky": ["danger", "defenselessness", "compulsion", "betrayal", "dullness"]
   },
   "ANSUZ": {
     "phonetic": "A",
     "number": 4,
-    "bright_stave": ["inspiration (enthusiasm)", "synthesis", "transformation", "words"],
-    "murky_stave": ["misunderstanding", "delusion", "manipulation by others", "boredom"]
+    "bright": ["inspiration (enthusiasm)", "synthesis", "transformation", "words"],
+    "murky": ["misunderstanding", "delusion", "manipulation by others", "boredom"]
   },
   "RAIDHO": {
     "phonetic": "R",
     "number": 5,
-    "bright_stave": ["rationality", "action", "justice", "ordered growth", "journey"],
-    "murky_stave": ["crisis", "rigidity", "stasis", "injustice", "irrationality"]
+    "bright": ["rationality", "action", "justice", "ordered growth", "journey"],
+    "murky": ["crisis", "rigidity", "stasis", "injustice", "irrationality"]
   },
   "KENAZ": {
     "phonetic": "K",
     "number": 6,
-    "bright_stave": ["technical ability", "inspiration", "creativity", "transformation", "offspring"],
-    "murky_stave": ["disease", "breakup", "inability", "lack of creativity"]
+    "bright": ["technical ability", "inspiration", "creativity", "transformation", "offspring"],
+    "murky": ["disease", "breakup", "inability", "lack of creativity"]
   },
   "GEBO": {
     "phonetic": "G",
     "number": 7,
-    "bright_stave": ["gift giving", "generosity", "magical exchange", "honor", "sacrifice"],
-    "murky_stave": ["influence-buying", "greed", "loneliness", "dependence", "over-sacrifice"]
+    "bright": ["gift giving", "generosity", "magical exchange", "honor", "sacrifice"],
+    "murky": ["influence-buying", "greed", "loneliness", "dependence", "over-sacrifice"]
   },
   "WUNJO": {
     "phonetic": "W",
     "number": 8,
-    "bright_stave": ["harmony", "joy", "fellowship", "prosperity"],
-    "murky_stave": ["stultification", "sorrow", "strife", "alienation"]
+    "bright": ["harmony", "joy", "fellowship", "prosperity"],
+    "murky": ["stultification", "sorrow", "strife", "alienation"]
   },
   "HAGALAZ": {
     "phonetic": "H",
     "number": 9,
-    "bright_stave": ["change according to ideals", "controlled crisis", "completion", "inner harmony"],
-    "murky_stave": ["catastrophe", "crisis", "stagnation", "loss of power"]
+    "bright": ["change according to ideals", "controlled crisis", "completion", "inner harmony"],
+    "murky": ["catastrophe", "crisis", "stagnation", "loss of power"]
   },
   "NAUTHIZ": {
     "phonetic": "N",
     "number": 10,
-    "bright_stave": ["resistance leading to strength", "recognition", "innovation", "need-fire", "self-reliance"],
-    "murky_stave": ["constraint of freedom", "distress", "toil", "drudgery", "laxity"]
+    "bright": ["resistance leading to strength", "recognition", "innovation", "need-fire", "self-reliance"],
+    "murky": ["constraint of freedom", "distress", "toil", "drudgery", "laxity"]
   },
   "ISA": {
     "phonetic": "I",
     "number": 11,
-    "bright_stave": ["concentrated self", "ego", "consciousness", "self-control", "unity"],
-    "murky_stave": ["ego-mania", "dullness", "blindness", "dissipation"]
+    "bright": ["concentrated self", "ego", "consciousness", "self-control", "unity"],
+    "murky": ["ego-mania", "dullness", "blindness", "dissipation"]
   },
   "JERA": {
     "phonetic": "J/Y",
     "number": 12,
-    "bright_stave": ["reward", "plenty", "peace", "proper timing"],
-    "murky_stave": ["repetition", "bad timing", "poverty", "conflict"]
+    "bright": ["reward", "plenty", "peace", "proper timing"],
+    "murky": ["repetition", "bad timing", "poverty", "conflict"]
   },
   "EIHWAZ": {
     "phonetic": "E/I",
     "number": 13,
-    "bright_stave": ["enlightenment", "endurance", "initiation", "protection"],
-    "murky_stave": ["confusion", "destruction", "dissatisfaction", "weakness"]
+    "bright": ["enlightenment", "endurance", "initiation", "protection"],
+    "murky": ["confusion", "destruction", "dissatisfaction", "weakness"]
   },
   "PERTHRO": {
     "phonetic": "P",
     "number": 14,
-    "bright_stave": ["good lot", "knowledge", "fellowship", "joy", "evolutionary change"],
-    "murky_stave": ["addiction", "stagnation", "loneliness", "malaise"]
+    "bright": ["good lot", "knowledge", "fellowship", "joy", "evolutionary change"],
+    "murky": ["addiction", "stagnation", "loneliness", "malaise"]
   },
   "ELHAZ": {
     "phonetic": "Z",
     "number": 15,
-    "bright_stave": ["connection with the gods", "awakening", "higher life", "protection"],
-    "murky_stave": ["hidden danger", "consumption by divine forces", "loss of divine link"]
+    "bright": ["connection with the gods", "awakening", "higher life", "protection"],
+    "murky": ["hidden danger", "consumption by divine forces", "loss of divine link"]
   },
   "SOWILO": {
     "phonetic": "S",
     "number": 16,
-    "bright_stave": ["guidance", "hope", "success", "goals achieved", "honor"],
-    "murky_stave": ["false goals", "bad counsel", "false success", "gullibility", "loss of goals"]
+    "bright": ["guidance", "hope", "success", "goals achieved", "honor"],
+    "murky": ["false goals", "bad counsel", "false success", "gullibility", "loss of goals"]
   },
   "TIWAZ": {
     "phonetic": "T",
     "number": 17,
-    "bright_stave": ["justice", "rationality", "self-sacrifice", "analysis"],
-    "murky_stave": ["mental paralysis", "over-analysis", "over-sacrifice", "injustice", "imbalance"]
+    "bright": ["justice", "rationality", "self-sacrifice", "analysis"],
+    "murky": ["mental paralysis", "over-analysis", "over-sacrifice", "injustice", "imbalance"]
   },
   "BERKANO": {
     "phonetic": "B",
     "number": 18,
-    "bright_stave": ["birth", "becoming", "life changes", "shelter", "liberation"],
-    "murky_stave": ["blurring of consciousness", "deceit", "sterility", "stagnation"]
+    "bright": ["birth", "becoming", "life changes", "shelter", "liberation"],
+    "murky: ["blurring of consciousness", "deceit", "sterility", "stagnation"]
   },
   "EHWAZ": {
     "phonetic": "E",
     "number": 19,
-    "bright_stave": ["harmony", "teamwork", "trust", "loyalty"],
-    "murky_stave": ["duplication", "disharmony", "mistrust", "betrayal"]
+    "bright": ["harmony", "teamwork", "trust", "loyalty"],
+    "murky": ["duplication", "disharmony", "mistrust", "betrayal"]
   },
   "MANNAZ": {
     "phonetic": "M",
     "number": 20,
-    "bright_stave": ["divine structure", "intelligence", "awareness", "social order"],
-    "murky_stave": ["depression", "mortality", "blindness", "self-delusion"]
+    "bright": ["divine structure", "intelligence", "awareness", "social order"],
+    "murky": ["depression", "mortality", "blindness", "self-delusion"]
   },
   "LAGUZ": {
     "phonetic": "L",
     "number": 21,
-    "bright_stave": ["life", "'water' journey", "sea of vitality", "sea of unconscious", "growth"],
-    "murky_stave": ["fear", "circular motion", "avoidance", "withering"]
+    "bright": ["life", "'water' journey", "sea of vitality", "sea of unconscious", "growth"],
+    "murky": ["fear", "circular motion", "avoidance", "withering"]
   },
   "INGWAZ": {
     "phonetic": "NG",
     "number": 22,
-    "bright_stave": ["rest stage", "internal growth", "gestation"],
-    "murky_stave": ["impotence", "scattering", "movement without change"]
+    "bright": ["rest stage", "internal growth", "gestation"],
+    "murky": ["impotence", "scattering", "movement without change"]
   },
   "DAGAZ": {
     "phonetic": "D",
     "number": 23,
-    "bright_stave": ["awakening", "awareness", "hope", "happiness", "the ideal"],
-    "murky_stave": ["blindness", "hopelessness"]
+    "bright": ["awakening", "awareness", "hope", "happiness", "the ideal"],
+    "murky": ["blindness", "hopelessness"]
   },
   "OTHALA": {
     "phonetic": "O",
     "number": 24,
-    "bright_stave": ["home", "group prosperity", "group order", "freedom", "productive interaction"],
-    "murky_stave": ["lack of customary order", "totalitarianism", "slavery", "poverty", "homelessness"]
+    "bright": ["home", "group prosperity", "group order", "freedom", "productive interaction"],
+    "murky": ["lack of customary order", "totalitarianism", "slavery", "poverty", "homelessness"]
   }
 }
 
 # list all rune names and ask user to select one; return all info on selected rune; enter 'back' to return to options
 def list_runes():
-  for rune in runes
-    print(runes)
+  print("\n")
+  for rune in runes:
+    print(rune)
+  list_sel = input("\nType name of rune to select: ").upper()
+  if list_sel in runes:
+    print(f"\nRune: {list_sel}")
+    for key, value in runes[list_sel].items():
+      print(f"{key.capitalize()}: {value}")
+      # for each value, print with comma separation
+    print("\n")
+  else:
+    print("\nThat's not a rune, silly.\n")
+      # go back and let them try again
 
 # randomly throw a single rune by name; ask user if they would like more info
   # if yes - return all info on rune
