bd7c1406e8f79fa261fa2ebf4e8353e8fc9e2ca9
TashiBelle <natasha.olivier224@gmail.com>
2025-04-08 04:50:13 -0500

    Update runes_dict.py

--- runes_dict.py
+++ runes_dict.py
@@ -6,157 +6,162 @@ runes = {
     "number": 1,
     "bright_stave": ["social success", "wealth energy", "foresight", "new beginnings"],
     "murky_stave": ["greed", "burnout", "atrophy", "poverty", "discord"]
-  }
+  },
   "URUZ": {
     "phonetic": "U",
     "number": 2,
     "bright_stave": ["strength", "defense", "tenacity", "freedom", "form", "health", "understanding"],
     "murky_stave": ["weakness", "obsession", "misdirected force", "domination by others", "sickness", "inconsistency", "ignorance"]
-  }
+  },
   "THURISAZ": {
     "phonetic": "TH",
     "number": 3,
     "bright_stave": ["reactive force", "directed force", "vital eroticism", "regenerative catalyst"],
     "murky_stave": ["danger", "defenselessness", "compulsion", "betrayal", "dullness"]
-  }
+  },
   "ANSUZ": {
     "phonetic": "A",
     "number": 4,
     "bright_stave": ["inspiration (enthusiasm)", "synthesis", "transformation", "words"],
     "murky_stave": ["misunderstanding", "delusion", "manipulation by others", "boredom"]
-  }
+  },
   "RAIDHO": {
     "phonetic": "R",
     "number": 5,
     "bright_stave": ["rationality", "action", "justice", "ordered growth", "journey"],
     "murky_stave": ["crisis", "rigidity", "stasis", "injustice", "irrationality"]
-  }
+  },
   "KENAZ": {
     "phonetic": "K",
     "number": 6,
     "bright_stave": ["technical ability", "inspiration", "creativity", "transformation", "offspring"],
     "murky_stave": ["disease", "breakup", "inability", "lack of creativity"]
-  }
+  },
   "GEBO": {
     "phonetic": "G",
     "number": 7,
     "bright_stave": ["gift giving", "generosity", "magical exchange", "honor", "sacrifice"],
     "murky_stave": ["influence-buying", "greed", "loneliness", "dependence", "over-sacrifice"]
-  }
+  },
   "WUNJO": {
     "phonetic": "W",
     "number": 8,
     "bright_stave": ["harmony", "joy", "fellowship", "prosperity"],
     "murky_stave": ["stultification", "sorrow", "strife", "alienation"]
-  }
+  },
   "HAGALAZ": {
     "phonetic": "H",
     "number": 9,
     "bright_stave": ["change according to ideals", "controlled crisis", "completion", "inner harmony"],
     "murky_stave": ["catastrophe", "crisis", "stagnation", "loss of power"]
-  }
+  },
   "NAUTHIZ": {
     "phonetic": "N",
     "number": 10,
     "bright_stave": ["resistance leading to strength", "recognition", "innovation", "need-fire", "self-reliance"],
     "murky_stave": ["constraint of freedom", "distress", "toil", "drudgery", "laxity"]
-  }
+  },
   "ISA": {
     "phonetic": "I",
     "number": 11,
     "bright_stave": ["concentrated self", "ego", "consciousness", "self-control", "unity"],
     "murky_stave": ["ego-mania", "dullness", "blindness", "dissipation"]
-  }
+  },
   "JERA": {
     "phonetic": "J/Y",
     "number": 12,
     "bright_stave": ["reward", "plenty", "peace", "proper timing"],
     "murky_stave": ["repetition", "bad timing", "poverty", "conflict"]
-  }
+  },
   "EIHWAZ": {
     "phonetic": "E/I",
     "number": 13,
     "bright_stave": ["enlightenment", "endurance", "initiation", "protection"],
     "murky_stave": ["confusion", "destruction", "dissatisfaction", "weakness"]
-  }
+  },
   "PERTHRO": {
     "phonetic": "P",
     "number": 14,
     "bright_stave": ["good lot", "knowledge", "fellowship", "joy", "evolutionary change"],
     "murky_stave": ["addiction", "stagnation", "loneliness", "malaise"]
-  }
+  },
   "ELHAZ": {
     "phonetic": "Z",
     "number": 15,
     "bright_stave": ["connection with the gods", "awakening", "higher life", "protection"],
     "murky_stave": ["hidden danger", "consumption by divine forces", "loss of divine link"]
-  }
+  },
   "SOWILO": {
     "phonetic": "S",
     "number": 16,
     "bright_stave": ["guidance", "hope", "success", "goals achieved", "honor"],
     "murky_stave": ["false goals", "bad counsel", "false success", "gullibility", "loss of goals"]
-  }
+  },
   "TIWAZ": {
     "phonetic": "T",
     "number": 17,
     "bright_stave": ["justice", "rationality", "self-sacrifice", "analysis"],
     "murky_stave": ["mental paralysis", "over-analysis", "over-sacrifice", "injustice", "imbalance"]
-  }
+  },
   "BERKANO": {
     "phonetic": "B",
     "number": 18,
     "bright_stave": ["birth", "becoming", "life changes", "shelter", "liberation"],
     "murky_stave": ["blurring of consciousness", "deceit", "sterility", "stagnation"]
-  }
+  },
   "EHWAZ": {
     "phonetic": "E",
     "number": 19,
     "bright_stave": ["harmony", "teamwork", "trust", "loyalty"],
     "murky_stave": ["duplication", "disharmony", "mistrust", "betrayal"]
-  }
+  },
   "MANNAZ": {
     "phonetic": "M",
     "number": 20,
     "bright_stave": ["divine structure", "intelligence", "awareness", "social order"],
     "murky_stave": ["depression", "mortality", "blindness", "self-delusion"]
-  }
+  },
   "LAGUZ": {
     "phonetic": "L",
     "number": 21,
     "bright_stave": ["life", "'water' journey", "sea of vitality", "sea of unconscious", "growth"],
     "murky_stave": ["fear", "circular motion", "avoidance", "withering"]
-  }
+  },
   "INGWAZ": {
     "phonetic": "NG",
     "number": 22,
     "bright_stave": ["rest stage", "internal growth", "gestation"],
     "murky_stave": ["impotence", "scattering", "movement without change"]
-  }
+  },
   "DAGAZ": {
     "phonetic": "D",
     "number": 23,
     "bright_stave": ["awakening", "awareness", "hope", "happiness", "the ideal"],
     "murky_stave": ["blindness", "hopelessness"]
-  }
+  },
   "OTHALA": {
     "phonetic": "O",
     "number": 24,
     "bright_stave": ["home", "group prosperity", "group order", "freedom", "productive interaction"],
     "murky_stave": ["lack of customary order", "totalitarianism", "slavery", "poverty", "homelessness"]
   }
+}
 
 # prompt user to input if they would like to pick from the list, throw 1, or throw 3; continue to correct line
-choice = input("Type '1' to view the list of runes. \nType '2' to cast one rune. \nType '3' to cast three runes. \n--> ")
-if choice == "1"
-  # list_runes()
-  print("Selectable list of runes coming soon!")
-elif choice == "2"
-  print("Single cast randomizer coming soon!")
-elif choice == "3"
-  print("Triple cast randomizer coming soon!")
-else:
-  print("No no no! \nType '1' to view the list of runes. \nType '2' to cast one rune. \nType '3' to cast three runes. \n--> ")
+while True: 
+  choice = input("Type '1' to view the list of runes. \nType '2' to cast one rune. \nType '3' to cast three runes. \n\n--> ")
+  if choice == "1":
+    # list_runes()
+     print("\nSelectable list of runes coming soon!\n")
+  elif choice == "2":
+    # cast1()
+    print("\nSingle cast randomizer coming soon!\n")
+  elif choice == "3":
+    # cast3()
+    print("\nTriple cast randomizer coming soon!\n")
+  else:
+    # restart from 'choice = input' ask
+    print("\nNo no no!\n")
 
 # list all rune names and ask user to select one; return all info on selected rune; enter 'back' to return to options
 
