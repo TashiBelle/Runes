278fb92850f2ee8a0fe264c281be53eed51f41f8
TashiBelle <natasha.olivier224@gmail.com>
2025-04-08 03:06:04 -0500

    Created runes_dict.py

---
+++ runes_dict.py
@@ -0,0 +1,169 @@
+runes = {
+  "FEHU": {
+    "phonetic": "F",
+    "number": 1,
+    "bright_stave": ["social success", "wealth energy", "foresight", "new beginnings"],
+    "murky_stave": ["greed", "burnout", "atrophy", "poverty", "discord"],
+    "sprite": "Mactintosh HD/Users/dawson/desktop/Custom Image Devices/fehuPNG.png"
+  }
+  "URUZ": {
+    "phonetic": "U",
+    "number": 2,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "THURISAZ": {
+    "phonetic": "TH",
+    "number": 3,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "ANSUZ": {
+    "phonetic": "A",
+    "number": 4,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "RAIDHO": {
+    "phonetic": "R",
+    "number": 5,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "KENAZ": {
+    "phonetic": "K",
+    "number": 6,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "GEBO": {
+    "phonetic": "G",
+    "number": 7,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "WUNJO": {
+    "phonetic": "W",
+    "number": 8,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "HAGALAZ": {
+    "phonetic": "H",
+    "number": 9,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "NAUTHIZ": {
+    "phonetic": "N",
+    "number": 10,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "ISA": {
+    "phonetic": "I",
+    "number": 11,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "JERA": {
+    "phonetic": "J/Y",
+    "number": 12,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "EIHWAZ": {
+    "phonetic": "E/I",
+    "number": 13,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "PERTHRO": {
+    "phonetic": "P",
+    "number": 14,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "ELHAZ": {
+    "phonetic": "Z",
+    "number": 15,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "SOWILO": {
+    "phonetic": "S",
+    "number": 16,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "TIWAZ": {
+    "phonetic": "T",
+    "number": 17,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "BERKANO": {
+    "phonetic": "B",
+    "number": 18,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "EHWAZ": {
+    "phonetic": "E",
+    "number": 19,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "MANNAZ": {
+    "phonetic": "M",
+    "number": 20,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "LAGUZ": {
+    "phonetic": "L",
+    "number": 21,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "INGWAZ": {
+    "phonetic": "NG",
+    "number": 22,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "DAGAZ": {
+    "phonetic": "D",
+    "number": 23,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
+  "OTHALA": {
+    "phonetic": "O",
+    "number": 24,
+    "bright_stave":
+    "murky_stave":
+    "sprite":
+  }
